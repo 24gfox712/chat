d077a2352278edf89e56c5e80dabf6a8d42df59d
24gfox712 <54448619+24gfox712@users.noreply.github.com>
2019-11-22 11:34:06 -0600

    Update ban.js

--- ban.js
+++ ban.js
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<script data-consolejs-channel="83c25abc-b559-45ba-0d49-9254a5ba2c25" src="https://remotejs.com/agent/agent.js"></script>
         <body>
                 <h1><b>You Have Been Banned</b></h1>
 </body>
